75faa6e887a7d0cd05ef626f2cb1ba9932f0e44c
Todd Carlson <tcarlson@carlsonsw.com>
2018-10-21 21:56:22 -0400

    completed line problem

--- 01-data-structures/01-introduction-to-data-structures/screen/pixel.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/pixel.rb
@@ -1,6 +1,6 @@
 # Pixels require three color values: red, green, and blue.
 # Pixel colors red, green, and blue values must be greater than zero and less than 255.
-# If  a value less than zero is specified, default to zero. If a value greater than 255 is specified, default to 255.
+# If a value less than zero is specified, default to zero. If a value greater than 255 is specified, default to 255.
 # Pixels also require a coordinate location: x and y.
 
 class Pixel
@@ -12,11 +12,23 @@ class Pixel
 
 
   def initialize(red, green, blue, x, y)
+    @red = validate_color(red)
+    @green = validate_color(green)
+    @blue = validate_color(blue)
+    @x = x
+    @y = y       
   end
 
   private
 
   def validate_color(color)
+    if color > 255
+      color = 255
+    elsif color < 0
+      color = 0
+    else
+      color      
+    end
   end
 
 end
