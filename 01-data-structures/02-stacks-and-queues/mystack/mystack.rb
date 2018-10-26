2396699b63789d326baaa0b6b1eed682191c157f
Todd Carlson <tcarlson@carlsonsw.com>
2018-10-26 13:51:51 -0400

    passing all but one mystack

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -1,3 +1,7 @@
+#Use the mystack_spec.rb spec file and mystack.rb skeleton file in the sep-assignments repository to create a stack class in Ruby.
+#Write push(element), pop, and empty? functions.
+#Keep a pointer to the top of the stack.
+
 class MyStack
   attr_accessor :top
 
@@ -7,11 +11,16 @@ class MyStack
   end
 
   def push(item)
+    @stack.insert(0, item)
+    self.top = @stack[0]
   end
 
   def pop
+    @stack.delete_at(0)
+    #self.top = @stack[0]
   end
 
   def empty?
+    @stack[0] ? false : true
   end
 end
