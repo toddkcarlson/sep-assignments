f35c65ea5be8e593835bdb7529f3ea05294c8329
Todd Carlson <tcarlson@carlsonsw.com>
2018-12-13 23:47:48 -0500

    some changes for KB

--- 01-data-structures/07-baconator/baconator.rb
+++ 01-data-structures/07-baconator/baconator.rb
@@ -1,15 +1,15 @@
 class Node
 
   attr_reader :name
-  attr_accessor :connection  
+  attr_accessor :connections 
   
   def initialize(name)
     @name = name
-    @connection = []
+    @connections = []
   end
   
   def add_edge(adjacent_node)
-    @connection << adjacent_node
+    @connections << adjacent_node
   end
   
 end
@@ -25,14 +25,14 @@ class Graph
   end
   
   def add_edge(node1, node2)
-    @nodes[node1].add_edge(@nodes[node2])
-    @nodes[node2].add_edge(@nodes[node1])
+    node1.add_edge(node2)
+    node2.add_edge(node1)
   end
 
   def find_kevin_bacon(starting_node)
     visited = []
     to_visit = []
-    path = []
+   
     # add root node to visited list and to_visit queue
     visited << starting_node
     to_visit << { :node => starting_node, :path => [starting_node.name]}
@@ -41,10 +41,10 @@ class Graph
       current = to_visit.shift
       current_node = current[:node]
       if current_node.name == "Kevin Bacon"
-        reutrn current_node.path
+        return current[:path]
       # node not found, add adjacent nodes to be visited if not already
       if current[:path].length <= 6
-        current_node.adjacent_nodes.each do |node|
+        current_node.connections.each do |node|
           if !visited.include?(current_node)
             visited << current_node
             to_visit <<  { :node => current_node, :path => current[:path] << current_node.name}
